50cbe1f2a15a1b2f49d9d8117a5e9dc937f2fbb5
Clément de Louvencourt <c.delouvencourt@nocturne.app>
2021-01-26 14:04:40 +0100

    add ferrari utils

---
+++ src/utils/TailleCube.java
@@ -0,0 +1,16 @@
+package utils;
+
+import java.io.*;
+
+public enum TailleCube
+{
+    grand, moyen, petit;
+
+    public static TailleCube getTaille(String taille) {
+        for (TailleCube t:TailleCube.values())
+            if (t.name().equalsIgnoreCase(taille))
+                return t;
+        return TailleCube.grand;
+    }
+
+}
